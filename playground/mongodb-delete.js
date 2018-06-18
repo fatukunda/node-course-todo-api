7a5c40a53b8cab7362d73837a5687e928e1f0186
Frank <fatukunda@hotmail.com>
2018-06-18 13:21:13 +0300

    Adding the update script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -6,14 +6,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to mongodb server');
     const db = client.db('TodoApp');
     //delete many
-    db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+    db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) =>
+
         console.log(result);
     });
     db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
         console.log(result);
     });
     //findOneAndDelete
-    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    db.collection('Todos').findOneAndDelete({_id: new ObjectId('"5b26c02edaa8c05025839be5')}).then((result) => {
         console.log(result);
     })
     //delete one
