81fb1537bcd8bc5adbe5bdd1a19663314162168e
Frank <fatukunda@hotmail.com>
2018-06-22 14:59:54 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectId} = require('mongodb');
 
 const app = express();
 app.use(bodyParser.json());
@@ -26,6 +27,21 @@ app.get('/todos', (req, res) => {
         res.send ({todos});
     }, (err) => {
         res.status(400).send(err);
+    })
+});
+//GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectId.isValid(id)){
+       return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+           return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((err) => {
+        res.status(400).send();
     })
 });
 
