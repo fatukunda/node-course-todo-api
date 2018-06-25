a3c3a131c1bc31288610413e8ba06c20f631ad31
Frank <fatukunda@hotmail.com>
2018-06-25 20:20:40 +0300

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const port = process.env.PORT || 3000;
 const {mongoose} = require('./db/mongoose');
@@ -55,6 +56,25 @@ app.delete('/todos/:id', (req, res) => {
             return res.status(404).send('No such todo');
         }
         res.send({ todo })
+    }).catch((err) => {
+        res.status(400).send();
+    })
+});
+//PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.send({todo});
     }).catch((err) => {
         res.status(400).send();
     })
